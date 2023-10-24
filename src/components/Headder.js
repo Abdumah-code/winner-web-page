7bac06eba716cc399ca7dd9fb1c8017a7442930b
abdu <abdumah2021fr@gmail.com>
2023-10-24 18:26:28 +0200

    initiall post

---
+++ src/components/Headder.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scss';
+
+const Header = () => {
+    return (
+        <div className="container">
+            <header>
+                <div className="logo">
+                    <h1>WINNER</h1>
+                    <p>LASH STUDIOS</p>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
+
